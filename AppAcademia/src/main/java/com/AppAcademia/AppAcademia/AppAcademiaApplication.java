c58e5a08c64e1874e9e032337ef37be32e64eeef
LuizManezzi <felipe_moraess@hotmail.com>
2024-02-19 22:10:20 -0300

    Alterado o banco para postgres e usando docker docker compose

--- AppAcademia/src/main/java/com/AppAcademia/AppAcademia/AppAcademiaApplication.java
+++ AppAcademia/src/main/java/com/AppAcademia/AppAcademia/AppAcademiaApplication.java
@@ -2,8 +2,12 @@ package com.AppAcademia.AppAcademia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.AppAcademia.AppAcademia")
+@EnableJpaRepositories(basePackages = "com.AppAcademia.AppAcademia")
 public class AppAcademiaApplication {
 
 	public static void main(String[] args) {
